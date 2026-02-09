553c342d9fc36f2b03800d565e823121ff36eb54
vaithisvarans-blip <vaithisvaran.s@gmail.com>
2026-02-09 23:01:36 +0530

    Update WSOsql.sql

--- WSOsql.sql
+++ WSOsql.sql
@@ -1,7 +1,9 @@
-WITH loanx_id_cte AS (
+WITH normalized_data AS (
+
     SELECT 
         weighted_average_all_in_rate,
-        loanx_id,
+        loanx_id AS instrument_id,
+        'LOANX_ID' AS id_type,
         SECURITY_ID,
         CUSIP_ID,
         business_date,
@@ -9,12 +11,13 @@ WITH loanx_id_cte AS (
         deal_sponsor
     FROM PROD_STAGING.PUBLIC.WSO_DAILY_SECURITYMASTER_ENRICHMENT
     WHERE COALESCE(loanx_id,'') <> ''
-),
 
-security_id_cte AS (
+    UNION ALL
+
     SELECT 
         weighted_average_all_in_rate,
-        loanx_id,
+        SECURITY_ID AS instrument_id,
+        'SECURITY_ID' AS id_type,
         SECURITY_ID,
         CUSIP_ID,
         business_date,
@@ -22,12 +25,13 @@ security_id_cte AS (
         deal_sponsor
     FROM PROD_STAGING.PUBLIC.WSO_DAILY_SECURITYMASTER_ENRICHMENT
     WHERE COALESCE(SECURITY_ID,'') <> ''
-),
 
-cusip_id_cte AS (
+    UNION ALL
+
     SELECT 
         weighted_average_all_in_rate,
-        loanx_id,
+        CUSIP_ID AS instrument_id,
+        'CUSIP_ID' AS id_type,
         SECURITY_ID,
         CUSIP_ID,
         business_date,
@@ -35,67 +39,23 @@ cusip_id_cte AS (
         deal_sponsor
     FROM PROD_STAGING.PUBLIC.WSO_DAILY_SECURITYMASTER_ENRICHMENT
     WHERE COALESCE(CUSIP_ID,'') <> ''
-),
-
-loanx_result AS (
-    SELECT 
-        a.weighted_average_all_in_rate,
-        a.loanx_id,
-        b.SECURITY_ID,
-        b.CUSIP_ID,
-        a.business_date,
-        a.lead_manager AS lead_manager_1,
-        b.lead_manager AS lead_manager_2,
-        a.deal_sponsor
-    FROM loanx_id_cte a
-    JOIN loanx_id_cte b
-        ON a.business_date = b.business_date
-        AND a.loanx_id = b.loanx_id
-        AND a.lead_manager <> b.lead_manager
-        AND COALESCE(a.lead_manager,'') <> ''
-        AND COALESCE(b.lead_manager,'') <> ''
-),
-
-security_result AS (
-    SELECT 
-        a.weighted_average_all_in_rate,
-        a.loanx_id,
-        b.SECURITY_ID,
-        b.CUSIP_ID,
-        a.business_date,
-        a.lead_manager AS lead_manager_1,
-        b.lead_manager AS lead_manager_2,
-        a.deal_sponsor
-    FROM security_id_cte a
-    JOIN security_id_cte b
-        ON a.business_date = b.business_date
-        AND a.SECURITY_ID = b.SECURITY_ID
-        AND a.lead_manager <> b.lead_manager
-        AND COALESCE(a.lead_manager,'') <> ''
-        AND COALESCE(b.lead_manager,'') <> ''
-),
-
-cusip_result AS (
-    SELECT 
-        a.weighted_average_all_in_rate,
-        a.loanx_id,
-        b.SECURITY_ID,
-        b.CUSIP_ID,
-        a.business_date,
-        a.lead_manager AS lead_manager_1,
-        b.lead_manager AS lead_manager_2,
-        a.deal_sponsor
-    FROM cusip_id_cte a
-    JOIN cusip_id_cte b
-        ON a.business_date = b.business_date
-        AND a.CUSIP_ID = b.CUSIP_ID
-        AND a.lead_manager <> b.lead_manager
-        AND COALESCE(a.lead_manager,'') <> ''
-        AND COALESCE(b.lead_manager,'') <> ''
 )
 
-SELECT * FROM loanx_result
-UNION ALL
-SELECT * FROM security_result
-UNION ALL
-SELECT * FROM cusip_result;
+SELECT
+    a.weighted_average_all_in_rate,
+    a.instrument_id,
+    a.id_type,
+    a.SECURITY_ID,
+    a.CUSIP_ID,
+    a.business_date,
+    a.lead_manager AS lead_manager_1,
+    b.lead_manager AS lead_manager_2,
+    a.deal_sponsor
+FROM normalized_data a
+JOIN normalized_data b
+    ON a.business_date = b.business_date
+    AND a.instrument_id = b.instrument_id
+    AND a.id_type = b.id_type
+    AND a.lead_manager <> b.lead_manager
+    AND COALESCE(a.lead_manager,'') <> ''
+    AND COALESCE(b.lead_manager,'') <> '';
