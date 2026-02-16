fe8e5f0aa5ea7cfdc8e863c8f78ef101b9e2dfe5
vaithisvarans-blip <vaithisvaran.s@gmail.com>
2026-02-16 20:43:40 +0530

    Update CDST Data compare.sql

--- CDST Data compare.sql
+++ CDST Data compare.sql
@@ -1,37 +1,36 @@
-WITH OLD_RESULT AS (
-    ---- paste OLD query here
-),
-
-NEW_RESULT AS (
-    ---- paste NEW query here
-),
-
-COMPARISON AS (
+with COMPARISON AS (
     SELECT
         COALESCE(o.business_date, n.business_date) AS business_date,
         COALESCE(o.aladdin_id, n.aladdin_id) AS aladdin_id,
 
+        n.LEAD_MANAGER,
+        o.LEAD_MANAGER,
+        n.DEAL_SPONSOR,
+        o.DEAL_SPONSOR,
+       
+
         o.coupon_rate AS old_coupon_rate,
         n.coupon_rate AS new_coupon_rate,
 
-        /* Difference with tolerance */
+    
         ABS(NVL(o.coupon_rate,0) - NVL(n.coupon_rate,0)) AS coupon_diff
 
-    FROM OLD_RESULT o
-    FULL OUTER JOIN NEW_RESULT n
+    FROM SANDBOX_01.VAITHI_DEV.STAGE_DIM_ASSET_DATA_COMPARE_BEFORE o
+    FULL OUTER JOIN SANDBOX_01.VAITHI_DEV.STAGE_DIM_ASSET_DATA_COMPARE_AFTER n
         ON o.business_date = n.business_date
         AND o.aladdin_id = n.aladdin_id
+       -- AND (COALESCE(n.LEAD_MANAGER,'') ='' 
+       -- AND COALESCE(n.DEAL_SPONSOR,'') ='')
+       
 )
 
-SELECT
-    business_date,
+SELECT --distinct aladdin_id
+   business_date,
     aladdin_id,
     old_coupon_rate,
     new_coupon_rate,
     coupon_diff
 FROM COMPARISON
-WHERE
-      old_coupon_rate IS NULL
-   OR new_coupon_rate IS NULL
-   OR coupon_diff > 0.0001   -- tolerance
-ORDER BY aladdin_id;
+WHERE coupon_diff > 0
+   and BUSINESS_DATE::date = '2026-02-11'   
+ORDER BY 1 desc;
